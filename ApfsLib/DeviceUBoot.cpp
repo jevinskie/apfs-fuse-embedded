553d99a0137e6dc7344686ab708b39035ea11e54
Jevin Sweval <jevinsweval@gmail.com>
2022-01-01 17:48:54 -0500

    DeviceUBoot stub

---
+++ ApfsLib/DeviceUBoot.cpp
@@ -0,0 +1,49 @@
+/*
+This file is part of apfs-fuse, a read-only implementation of APFS
+(Apple File System) for FUSE.
+Copyright (C) 2017 Simon Gander
+Copyright (C) 2021 Jevin Sweval
+
+Apfs-fuse is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Apfs-fuse is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with apfs-fuse.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "DeviceUBoot.h"
+
+DeviceUBoot::DeviceUBoot()
+{
+}
+
+DeviceUBoot::~DeviceUBoot()
+{
+    Close();
+}
+
+bool DeviceUBoot::Open(const char *name)
+{
+    return false;
+}
+
+void DeviceUBoot::Close()
+{
+}
+
+bool DeviceUBoot::Read(void * data, uint64_t offs, uint64_t len)
+{
+    return false;
+}
+
+uint64_t DeviceUBoot::GetSize() const
+{
+    return 0;
+}

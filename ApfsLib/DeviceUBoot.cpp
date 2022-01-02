90861fbfed6e28af05ebfc2ce0685d3fddcccc41
Jevin Sweval <jevinsweval@gmail.com>
2022-01-02 08:03:24 -0500

    int typdefs

--- ApfsLib/DeviceUBoot.cpp
+++ ApfsLib/DeviceUBoot.cpp
@@ -24,6 +24,17 @@ along with apfs-fuse.  If not, see <http://www.gnu.org/licenses/>.
 
 extern "C" {
 
+typedef uint8_t  u8;
+typedef  int8_t  s8;
+typedef uint16_t u16;
+typedef  int16_t s16;
+typedef uint32_t u32;
+typedef  int32_t s32;
+typedef uint64_t u64;
+typedef  int64_t s64;
+typedef unsigned long ulong;
+typedef signed long slong;
+
 #include <blk.h>
 
 } // extern "C"
